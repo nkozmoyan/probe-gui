32509d2f33a58e95da9c134abe8a9c0f3e78d907
Narek <narek@35mm.am>
2018-09-16 19:16:23 +0400

    production api URL

--- src/app/probe/probe-service.ts
+++ src/app/probe/probe-service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { timer, Observable } from 'rxjs';
 import { flatMap } from 'rxjs/operators';
-
+import { environment } from '../../environments/environment';
 
 export interface Config {
     heroesUrl: string;
@@ -12,78 +12,81 @@ export interface Config {
 @Injectable()
 export class ProbeService {
 
-    constructor(private http:HttpClient){ }
-
-    timer = timer(0, 10000);
+    constructor(private http:HttpClient){ 
+        
+    }
+    
+    private apiUrl = environment.apiUrl
+    private timer = timer(0, 10000);
 
     createProbe(probe:any){
-        return this.http.post('http://localhost:3050/api/probes/', probe);
+        return this.http.post(this.apiUrl+'/probes/', probe);
     }
     
    updateProbe(id:any,probe:any){
-        return this.http.put('http://localhost:3050/api/probes/'+id, probe);
+        return this.http.put(this.apiUrl + '/probes/'+id, probe);
     }
 
 
     listProbes(){
-        return this.http.get('http://localhost:3050/api/probes/');
+        return this.http.get(this.apiUrl + '/probes/');
     }
 
     getProbeResults(id:any){
-        return this.timer.pipe(flatMap((i) => this.http.get('http://localhost:3050/api/probes/'+id+'/results')))
+        return this.timer.pipe(flatMap((i) => this.http.get(this.apiUrl + '/probes/'+id+'/results')))
     }
 
     describeProbe(id:any){
-        return this.http.get('http://localhost:3050/api/probes/'+id)
+        return this.http.get(this.apiUrl + '/probes/'+id)
     }
 
     deleteProbe(id:any){
-        return this.http.delete('http://localhost:3050/api/probes/'+id);
+        return this.http.delete(this.apiUrl + '/probes/'+id);
     }
 
      // *************************************
 
     createNotifyPolicy(probe:any){
-        return this.http.post('http://localhost:3050/api/notification-policies/', probe);
+        return this.http.post(this.apiUrl + '/notification-policies/', probe);
     }
 
     describeNotifyPolicy(id:any){
-        return this.http.get('http://localhost:3050/api/notification-policies/'+id)
+        return this.http.get(this.apiUrl + '/notification-policies/'+id)
     }
     
     updateNotifyPolicy(id:any,probe:any){
-        return this.http.put('http://localhost:3050/api/notification-policies/'+id, probe);
+        return this.http.put(this.apiUrl + '/notification-policies/'+id, probe);
     }
 
     deleteNotifyPolicy(id:any){
-        return this.http.delete('http://localhost:3050/api/notification-policies/'+id);
+        return this.http.delete(this.apiUrl + '/notification-policies/'+id);
     }
 
     
     listNotifyPolicies(){
-        return this.http.get('http://localhost:3050/api/notification-policies/');
+        return this.http.get(this.apiUrl + '/notification-policies/');
     }    
 
     // *************************************
 
     createNotifyChannel(probe:any){
-        return this.http.post('http://localhost:3050/api/notification-channels/', probe);
+        return this.http.post(this.apiUrl + '/notification-channels/', probe);
     }
 
     describeNotifyChannel(id:any){
-        return this.http.get('http://localhost:3050/api/notification-channels/'+id)
+        return this.http.get(this.apiUrl + '/notification-channels/'+id)
     }
     
     updateNotifyChannel(id:any,probe:any){
-        return this.http.put('http://localhost:3050/api/notification-channels/'+id, probe);
+        return this.http.put(this.apiUrl + '/notification-channels/'+id, probe);
     }
 
     deleteNotifyChannel(id:any){
-        return this.http.delete('http://localhost:3050/api/notification-channels/'+id);
+        return this.http.delete(this.apiUrl + '/notification-channels/'+id);
     }
 
     
     listNotifyChannels(){
-        return this.http.get('http://localhost:3050/api/notification-channels/');
+        return this.http.get(this.apiUrl + '/notification-channels/');
     }    
 }
