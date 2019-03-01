9dd9aa72fbf663625e00b41cf7909f92f8795316
Narek Kozmoyan <narek@Lanas-MacBook-Pro-2.local>
2019-03-02 01:24:09 +0400

    Date range selectors and aggregated data support

--- src/app/probe/probe.component.ts
+++ src/app/probe/probe.component.ts
@@ -1,25 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProbeService } from './probe-service';
 import { Probe } from './probe';
 import { ActivatedRoute } from '@angular/router';
 import { Pipe, PipeTransform } from '@angular/core';
+import { BaseChartDirective } from 'ng2-charts/ng2-charts';
+
 
 @Pipe({
   name: 'reverse'
 })
+
 @Component({
   selector: 'app-probe',
   templateUrl: './probe.component.html',
   styleUrls: ['./probe.component.css']
 })
 export class ProbeComponent implements OnInit {
+  @ViewChild("baseChart") chart: BaseChartDirective;
 
-  constructor(private probeService:ProbeService, private router: ActivatedRoute) { }
-  
+  public probe:any = {};
+
+  constructor(private probeService:ProbeService, private router: ActivatedRoute) {
+
+    this.probeService.describeProbe(this.probe_id).subscribe(response=>{
+      this.probe = response;
+  }, error => {
+      console.log(error)
+    })
+
+  }
+
   // lineChart
-  public lineChartData:Array<any> = [{},{},{},{},{}];
+  public lineChartData:Array<any>= [{},{}];
   public lineChartLabels:Array<any>;
   public lineChartOptions:any = { 
+  
     responsive: true,
     
     scales: {
@@ -33,7 +48,7 @@ export class ProbeComponent implements OnInit {
       }],
 
       yAxes: [{
-        stacked: true,
+        
         scaleLabel: {
           display: true,
           labelString: 'Value'
@@ -107,37 +122,94 @@ export class ProbeComponent implements OnInit {
 
   public setTimeRange(timeRange:Number){
     this.timeRange = timeRange;
+    this.reloadIsRequired = true;
     this.restartSubscription();
+    
   }
 
   public setLocation(location:String){
     this.locName = location;
+    this.reloadIsRequired = true;
     this.restartSubscription();
+
   }
+
   probe_id = this.router.snapshot.paramMap.get('id');
   
   public probeResults:Array<Object>;
-  public handleResponse(response:any){
+
+  public handleResponseForOverview(response:any){
+
+    this.lineChartOptions.scales.yAxes = [{
+      stacked:false
+    }]
+
+    let chartData = {};
     
-    let responseProps = Object.keys(response);
-    let chartData = {dns:[],wait:[],tcp:[],firstByte:[],download:[]};
+    let responseProps = Object.keys(response); // Making an array from fetched JSON.
+    let responseArray:any[] = []; // intermediate variable for keeping array
 
-    let respArr = [];
-
-    for (let prop of responseProps) { 
+    for (let i of responseProps) { 
       
-      respArr.push(response[prop]);
+      responseArray.push(response[i]);
 
-      if(typeof response[prop]['timingPhases'] !== 'undefined'){
-        chartData.dns.push({ t:response[prop]['probeTime'], y:response[prop]['timingPhases']['dns']});
-        chartData.wait.push({ t:response[prop]['probeTime'], y:response[prop]['timingPhases']['wait']});
-        chartData.tcp.push({ t:response[prop]['probeTime'], y:response[prop]['timingPhases']['tcp']});
-        chartData.firstByte.push({ t:response[prop]['probeTime'], y:response[prop]['timingPhases']['firstByte']});
-        chartData.download.push({ t:response[prop]['probeTime'], y:response[prop]['timingPhases']['download']});
+      if (!chartData.hasOwnProperty(response[i]['locName'])){
+        chartData[response[i]['locName']] = [];
+      } else {
+        chartData[response[i]['locName']].push({ t:response[i]['probeTime'], y:response[i]['responseTime']});
+      }
+    
+    }
+    
+    this.probeResults = responseArray.reverse();
+    this.lineChartData.length = 0;
+    this.probe.locations.forEach((location: any) => {
+      this.lineChartData.push({data: chartData[location], label:location })
+    });
+
+    this.reloadChart();
+
+    
+  }
+  private reloadIsRequired=true;
+  private reloadChart() {
+    if (this.chart !== undefined && this.reloadIsRequired) {
+       this.chart.chart.destroy();
+       this.chart.chart = 0;
+
+       this.chart.datasets = this.lineChartData;
+       this.chart.labels = this.lineChartLabels;
+       this.chart.ngOnInit();
+
+       this.reloadIsRequired=false;
+    }
+}
+
+  public handleResponse(response:any){
+
+    this.lineChartOptions.scales.yAxes = [{
+      stacked:true
+    }]
+
+    let chartData = {dns:[],wait:[],tcp:[],firstByte:[],download:[]};
+    
+    let responseProps = Object.keys(response); // Making an array from fetched JSON.
+    let responseArray:any[] = []; // intermediate variable for keeping array
+
+    for (let i of responseProps) { 
+      
+      responseArray.push(response[i]);
+
+      if(typeof response[i]['timingPhases'] !== 'undefined'){
+        chartData.dns.push({ t:response[i]['probeTime'], y:response[i]['timingPhases']['dns']});
+        chartData.wait.push({ t:response[i]['probeTime'], y:response[i]['timingPhases']['wait']});
+        chartData.tcp.push({ t:response[i]['probeTime'], y:response[i]['timingPhases']['tcp']});
+        chartData.firstByte.push({ t:response[i]['probeTime'], y:response[i]['timingPhases']['firstByte']});
+        chartData.download.push({ t:response[i]['probeTime'], y:response[i]['timingPhases']['download']});
       }   
     }
     
-    this.probeResults = respArr.reverse();
+    this.probeResults = responseArray.reverse();
 
     this.lineChartData = [
      { data: chartData.dns, label:"DNS" },
@@ -146,16 +218,23 @@ export class ProbeComponent implements OnInit {
      { data: chartData.firstByte, label:"First Byte" },
      { data: chartData.download, label:"Download" }
     ];
-    console.log( this.lineChartData);
+    console.log(this.lineChartData);
+
+    this.reloadChart();
+
+   
   }
 
   private subscription;
 
   private initSubscription(){
+
+    let func = this.locName ? this.handleResponse : this.handleResponseForOverview;
+
     this.subscription = this.probeService.getProbeResults(this.probe_id, this.timeRange, this.locName).subscribe(
-      this.handleResponse.bind(this), error => {
-        console.log(error)
+      func.bind(this), error => {
       })
+      
   }
 
   private stopSubscription(){
@@ -173,7 +252,6 @@ export class ProbeComponent implements OnInit {
 
   ngOnDestroy(){
       this.stopSubscription();
-      console.log("hey");
   }
 }
 
